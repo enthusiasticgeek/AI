5a2bb76451e50a055b26dc6d57149781ffa8e033
enthusiasticgeek <enthusiasticgeek@gmail.com>
2020-01-12 08:19:27 -0500

    linear regressor

--- tutorialspoint/linear_regressor.py
+++ tutorialspoint/linear_regressor.py
@@ -30,6 +30,13 @@ reg_linear.fit(X_train, y_train)
 #predict with test data
 y_test_pred = reg_linear.predict(X_test)
 
+print("Performance of Linear regressor:")
+print("Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred), 2))
+print("Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred), 2))
+print("Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred), 2))
+print("Explain variance score =", round(sm.explained_variance_score(y_test, y_test_pred),2))
+print("R2 score =", round(sm.r2_score(y_test, y_test_pred), 2))
+
 #plot and visualize
 plt.scatter(X_test, y_test, color = 'red')
 plt.plot(X_test, y_test_pred, color = 'black', linewidth = 2)
